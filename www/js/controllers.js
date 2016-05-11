8bc404fdcfa4c1ee7fc0e038a208cf0517255ffe
TentriSimbolon <tentrimay.simbolon@gmail.com>
2016-05-11 19:32:03 +0700

    Tentri Commit 1

---
+++ www/js/controllers.js
@@ -0,0 +1,9 @@
+var app = angular.module('App.controllers', [])
+
+app.controller('AppCtrl', function() {
+
+})
+
+app.controller('HomeCtrl', function($scope) { 
+	$scope.title = "D'besto"; 
+})
---
+++ www/js/routers.js
@@ -0,0 +1,23 @@
+var app = angular.module('App.routers', [])
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('app', {
+		url: '/app',
+		abstract: true,
+		templateUrl: 'views/partials/menu.html',
+		controller: 'AppCtrl'
+	})
+
+	.state('app.home', {
+			url: '/home',
+			views: {
+					'menuContent': {
+							templateUrl: 'views/pages/home.html',
+							controller: 'HomeCtrl'
+					}
+				}
+			})
+
+		$urlRouterProvider.otherwise('/app/home');
+ 
+	})
